e6ffdf60f6289efc8d9d47e619289f896c955e63
Eeshaan Sarda <sardaeeshaan@gmail.com>
2024-07-05 20:04:26 +0545

    added dummy image

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -19,12 +19,12 @@ const HomeScreen = () => {
         {
           title: 'Software Development',
           skills: ['Coding', 'Testing', 'Debugging'],
-          imageUrl: '',
+          imageUrl: 'https://s3-alpha-sig.figma.com/img/4bfa/d134/035bf7db4ec485a44e5a8384c59da98d?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DTJkZGiT3T67u9jooPz7bO4qMY8RXRwV8BCONlkV~TmMtVmPDd6rolRRIAl~QwImd03aLPc48KouVn6Zrnok9sWcQpH-DX3Rkt7gxup90HCzcYgYrI39y4ST4AINP-HnBWtH9K1UDb-0B3JHLOU6A7iTSxfV0YxAn9NBg6rfq6lFDfz62BIBqC6Vn3IK35NHMBYRXGpmp0dLf1wGQvbIpKHpwswHS2PX00274FE203pjwyoNjdCc3WFtRwfYFycA~JjpuwHer78YON7tzGeEjAGTCN8XjJCVmyOrvQLK~A9VPw4mqrbPfKv4-4XndSVfH3rhgyuRrkYZSz4JOEqLnQ__',
         },
         {
           title: 'Game Systems',
           skills: ['Game Design', 'Programming', 'Graphics'],
-          imageUrl: '',
+          imageUrl: 'https://s3-alpha-sig.figma.com/img/7fe2/7130/cb72fb6294029e285151457c1fd2835d?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BbHXxSD2bWhHw~IvECWx7sRmjuZFLEPclIXfD2B~h80kJgPusckwForE-TwQIF4DHQRyRyG44d7ROLI1mCEeApXu2A1Y9daq0CxIVGmaKN~jYT7glukFxMlU~uI2P2ZwCE-xt0LQ~wHy7bz-c~JHsV32zKN2jXV4cWOuvRbEi3-tr--Ynafo7ztrubxvUQwAkly1IP84b-pQHGJccj5D2-C6W1~uEb8zQM3AEx1JDum6ALJtrLu9T4tMeFGUlZ-h3IpoknkELQE-rm69ikRSxltYp3SRDpV~gHe8T6SV-MS3ptuyNpkc6~HFzlfYI2p5aTFHkdtzlYs4f~jtqKm2cg__',
         },
       ],
     },
@@ -34,12 +34,12 @@ const HomeScreen = () => {
         {
           title: 'Network and Infrastructure',
           skills: ['Coding', 'Testing', 'Debugging'],
-          imageUrl: '',
+          imageUrl: 'https://s3-alpha-sig.figma.com/img/4bfa/d134/035bf7db4ec485a44e5a8384c59da98d?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DTJkZGiT3T67u9jooPz7bO4qMY8RXRwV8BCONlkV~TmMtVmPDd6rolRRIAl~QwImd03aLPc48KouVn6Zrnok9sWcQpH-DX3Rkt7gxup90HCzcYgYrI39y4ST4AINP-HnBWtH9K1UDb-0B3JHLOU6A7iTSxfV0YxAn9NBg6rfq6lFDfz62BIBqC6Vn3IK35NHMBYRXGpmp0dLf1wGQvbIpKHpwswHS2PX00274FE203pjwyoNjdCc3WFtRwfYFycA~JjpuwHer78YON7tzGeEjAGTCN8XjJCVmyOrvQLK~A9VPw4mqrbPfKv4-4XndSVfH3rhgyuRrkYZSz4JOEqLnQ__',
         },
         {
           title: 'Risk Management',
           skills: ['Game Design', 'Programming', 'Graphics'],
-          imageUrl: '',
+          imageUrl: 'https://s3-alpha-sig.figma.com/img/7fe2/7130/cb72fb6294029e285151457c1fd2835d?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BbHXxSD2bWhHw~IvECWx7sRmjuZFLEPclIXfD2B~h80kJgPusckwForE-TwQIF4DHQRyRyG44d7ROLI1mCEeApXu2A1Y9daq0CxIVGmaKN~jYT7glukFxMlU~uI2P2ZwCE-xt0LQ~wHy7bz-c~JHsV32zKN2jXV4cWOuvRbEi3-tr--Ynafo7ztrubxvUQwAkly1IP84b-pQHGJccj5D2-C6W1~uEb8zQM3AEx1JDum6ALJtrLu9T4tMeFGUlZ-h3IpoknkELQE-rm69ikRSxltYp3SRDpV~gHe8T6SV-MS3ptuyNpkc6~HFzlfYI2p5aTFHkdtzlYs4f~jtqKm2cg__',
         },
       ],
     },
